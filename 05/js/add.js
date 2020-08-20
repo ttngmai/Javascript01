5f3c1ab448f7964c49d7289d364fdf0b2e5d21c5
ttngmai <frozenfruit24@gmail.com>
2020-08-20 22:22:11 +0900

    2020-08-20

---
+++ 05/js/add.js
@@ -0,0 +1,6 @@
+function addNumber() {
+    var num1 = parseInt(prompt("첫 번째 숫자는?"));
+	var num2 = parseInt(prompt("두 번째 숫자는?"));
+    var sum = num1 + num2;
+    alert("두 수를 더한 값은 " + sum + "입니다.");
+}
